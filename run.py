3bf43b4b9ec3079b4f4ab2f0f124c6c813f59997
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2024-12-05 13:02:51 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
-# Run the app
-if __name__=="__main__":
+from app import app, db
+from app import views  # Import views to ensure all routes are loaded before running the app
+
+if __name__ == "__main__":
+    with app.app_context():
+        # Ensure all tables are created before running the application
+        db.create_all()
+
+    # Run the app
     app.run(host='127.0.0.1', port=1300, debug=True)
